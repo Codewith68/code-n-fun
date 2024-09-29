a7bc8135f30a6663cdbbf36efbaf7120b99af03e
subrat palei <your-email@example.com>
2024-09-29 14:17:14 +0530

    sklak

---
+++ web dev/javascript/FUNCTION/function11.js
@@ -0,0 +1,18 @@
+const s1="subr\nat";
+let s2='subratt\tata';
+let s3=`here is an back\ttick example`;
+let s8="here is an example \"this is an example\"";
+console.log(s1);
+console.log(s2);
+console.log(s8);
+
+
+const str="abc";
+const strr="bca";
+console.log(str+strr); // this is an example of stirng concatination using the + operator
+
+
+const ste="SUBRAT";
+const sty="palei";
+console.log(ste.concat("-",sty));
+console.log(ste.toLowerCase());
