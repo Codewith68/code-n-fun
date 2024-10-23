5731d63bb257d387b70543a5df654ee5a1ff204c
subrat palei <your-email@example.com>
2024-10-23 08:36:31 +0530

    sdgs

--- web dev/javascript/variable.js
+++ web dev/javascript/variable.js
@@ -10,3 +10,12 @@ var e=5;
 console.log(e);
 f=19;
 console.log(f);
+// function fun(){
+//      console.log(x);
+//     {
+//      //console.log(x);
+//       let x=5;
+//       console.log(x);
+//     }
+//   }
+//    fun();
