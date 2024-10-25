adb76900cbc65a9fd40cacd68525273fe3028987
subrat palei <your-email@example.com>
2024-10-25 15:10:29 +0530

    uh

--- web dev/javascript/OOPS USING JS/classdemo.js
+++ web dev/javascript/OOPS USING JS/classdemo.js
@@ -1,3 +1,27 @@
 class product {
-    
+     
+
+
+    name;
+    Price;
+    Catagory;
+    Descripton;
+    Rating;
+
+
+    addToCart(){
+        console.log("product added to cart");
+    }
+
+    RemoveFromCart(){
+        console.log("product Removed to cart");
+    }
+
+    DisplayProduct(){
+        console.log("Product Displayed");
+    }
+
+    BuyProduct(){
+        console.log("Product Bought");
+    }
 }
