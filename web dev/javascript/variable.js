1a2d3194f3a9bef2ca50f6322e73e9dc770ed021
subrat palei <your-email@example.com>
2024-12-20 18:23:23 +0530

    chamges

--- web dev/javascript/variable.js
+++ web dev/javascript/variable.js
@@ -1,15 +1,15 @@
-let x=5;
-console.log(x);
+// let x=5;
+// console.log(x);
  
 
-const b=4;
-console.log(b);
+// const b=4;
+// console.log(b);
 
 
-var e=5;
-console.log(e);
-f=19;
-console.log(f);
+// var e=5;
+// console.log(e);
+// f=19;
+// console.log(f);
 // function fun(){
 //      console.log(x);
 //     {
@@ -19,3 +19,6 @@ console.log(f);
 //     }
 //   }
 //    fun();
+let num=1;
+ num=2;
+console.log(num);
