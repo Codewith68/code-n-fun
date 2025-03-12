263b3b8dbd35373a8c4665a01651ef669d4ddfbd
Subrat Palei <subratpaleibabul@gmail.com>
2025-03-12 14:13:25 +0530

    Update shortcircuiting.js.js
    new element

--- web dev/javascript/js basic fundda/shortcircuiting.js.js
+++ web dev/javascript/js basic fundda/shortcircuiting.js.js
@@ -5,7 +5,7 @@ console.log("" && -0);
 
 // this concept is known as shortcircuting and it only act on the logical operators or in a boolean values
 
-console.log(10 || 30);
+console.log(20 || 30);
 console.log(0 || 30);
 console.log(40 || null);
 console.log("" || -0);
