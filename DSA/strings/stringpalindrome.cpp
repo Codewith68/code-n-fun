24eea7d70cb68d1f03818feccac41ee8e7cddb50
subrat palei <subratpaleibabul@gmail.com>
2026-01-15 00:59:26 +0530

    adding last html and css project files

--- DSA/strings/stringpalindrome.cpp
+++ DSA/strings/stringpalindrome.cpp
@@ -1,21 +1,21 @@
 #include<iostream>
 #include<string>
-#include<vector>
 #include<algorithm>
 using namespace std;
 int main(){
-    string s;
-    cout<<"enter a string ";
-    getline(cin,s);
-    int i=0;
-    int j=s.length()-1;
-     bool flag=false;
-     for(int i=0;i<=s.size()/2;i++){
-        if(s[i]==s[j]){ flag=true;
-        break;}
-        i++;
-        j--;
-     }
-     if(flag==true) cout<<"string is palindrome";
-     else cout<<"string is not palindrome";
+   string s;
+   cout<<"Enter the string you want to check for plaindrome";
+   getline(cin,s);
+   int j=s.length()-1;
+
+   bool flag=true;
+   for(int i=0;i<s.length()/2;i++){
+      if(s[i]!=s[j]){
+         flag=false;
+         break;
+      }
+      j--;
+   }
+   if(flag==true) cout<<"palindrome";
+   else cout<<"not a palindrome";
 }
